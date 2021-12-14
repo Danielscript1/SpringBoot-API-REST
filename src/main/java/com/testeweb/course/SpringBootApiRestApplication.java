015b48fd9751bdda9ce5ebe42a3aed1b46b0fade
Danielscript1 <danielsousagit@gmail.com>
2021-12-14 10:42:55 -0300

    instalandoDepedenciaMaven

---
+++ src/main/java/com/testeweb/course/SpringBootApiRestApplication.java
@@ -0,0 +1,13 @@
+package com.testeweb.course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootApiRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootApiRestApplication.class, args);
+	}
+
+}
