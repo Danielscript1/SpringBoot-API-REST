1be007df57e1c0cb7e0f408fda291d86c1777130
Danielscript1 <danielsousagit@gmail.com>
2021-12-16 11:59:41 -0300

    BuscandoPeloId

--- src/main/java/com/testeweb/course/controller/TopicosController.java
+++ src/main/java/com/testeweb/course/controller/TopicosController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -46,6 +47,12 @@ public class TopicosController {
 		topicoRepository.save(topico);
 		URI uri = uriBuilder.path("/topicos/{id}").buildAndExpand(topico.getId()).toUri();
 		return ResponseEntity.created(uri).body(new TopicoDto(topico));
+	}
+	//buscandoResultadosPeloId
+	@GetMapping("/{id}")
+	public TopicoDto detalhar(@PathVariable Long id) {
+		Topico topico = topicoRepository.getById(id);
+		return new TopicoDto(topico);
 	}
 	
 }
